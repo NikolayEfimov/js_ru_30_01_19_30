da0707e33cec1c3634b6f56f722767d1cb7a218f
Roman <kaboom.ua@gmail.com>
2017-02-08 22:28:18 +0200

    Update toggleOpenArticle.js

--- src/decorators/toggleOpenArticle.js
+++ src/decorators/toggleOpenArticle.js
@@ -3,12 +3,13 @@ import React from "react";
 export default (Component)=> class WrappedComponent extends Component {
 
     state = {
+        //Не привязывайся к названиям сущностей, вся суть декораторов в универсальности. Сделай openItemId
         openArticleId: null
     }
 
     toggleOpenArticle = openArticleId => ev => {
         ev && ev.preventDefault && ev.preventDefault()
-
+        //много лишних условий
         this.state.openArticleId == null ||
         (this.state.openArticleId != null && this.state.openArticleId != openArticleId) ?
             this.setState({openArticleId: openArticleId}) :
