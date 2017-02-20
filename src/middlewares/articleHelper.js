ebf0b4625f9a16c7b3069941842bd175ed58b873
Roman <kaboom.ua@gmail.com>
2017-02-20 11:04:57 +0200

    Update articleHelper.js

--- src/middlewares/articleHelper.js
+++ src/middlewares/articleHelper.js
@@ -2,6 +2,7 @@ import {ADD_NEW_COMMENT} from "../constants";
 
 export default store => next => action => {
     const {type, ...rest} = action
+	//через мидлвары будут проходить все экшины, суть в том, что делать их нужно максимально реюзабильными. Не завязывайся на один экшин
     if (type != ADD_NEW_COMMENT) return next(action)
 
     let guid = Math.random().toString(16).slice(2)
